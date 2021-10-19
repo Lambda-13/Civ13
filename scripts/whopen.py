bcffeba9c31b9658629cade7788a36f7e2643dee
Sanec <xxxxxxxxxxxxxxxxxxxx.2002@gmail.com>
2021-10-19 15:33:10 +0700

    Работай блядина

--- scripts/whopen.py
+++ scripts/whopen.py
@@ -1,12 +1,18 @@
 import os
+import shutil
+import time
 import requests
 
-handle = open("token.txt", "r")
-token = handle.read()
-print("Запускаем...")
-handle.close()
+currdir = os.path.dirname(os.path.abspath(__file__))
+lines = open(os.path.join(currdir, "token.txt"))
+all_lines = lines.readlines()
+token = all_lines[1]
+token = token.replace("\n", "")
+token = token.replace("", "")
+print(token)
 
 from discord import Webhook, RequestsWebhookAdapter
 
 webhook = Webhook.from_url(token, adapter=RequestsWebhookAdapter())
 webhook.send("<@&896361299057983519> запустились, ссылка <byond://195.140.146.172:15/>")
+
